679e9f35c4e5cc2aa102114dc6e03384c710ced5
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2013-09-20 12:33:35 -0400

    updated

--- getrootservers.py
+++ getrootservers.py
@@ -22,18 +22,36 @@ __status__ = "Production"
 
 """ Set URL """
 url = "http://www.root-servers.org/"
-soup = BeautifulSoup(urllib2.urlopen(url))
 
-""" Scrap the Page """
-servers = soup.findAll('td', attrs ={'id':'server'})
-operators = soup.findAll('td', attrs = {'id':'operator'})
-ips = soup.findAll('td', attrs = {'id':'ips'})
+def getrootservers(url, verbose=False):
+        """
+        Scrape root-servers.org for rootservers
 
-""" Join The Finds """
-for server, operator, ip in zip(servers,operators,ips):
-    rootserver = (server.find('a') or server).contents[0]
-    operator = operator.contents[0]
-    ip = ip.contents[0].split(":")[1]
+        Takes 2 args, url and verbose[true|false=default]
+        """
+        soup = BeautifulSoup(urllib2.urlopen(url))
 
-    """ Print Out What We Found """
-    print '{0}.root-servers.org is operated by {1} and exists at {2}'.format(rootserver,operator,ip)
+        """ Scrap the Page """
+        #servers = soup.findAll('td', attrs ={'id':'server'})
+        #operators = soup.findAll('td', attrs = {'id':'operator'})
+        #ips = soup.findAll('td', attrs = {'id':'ips'})
+
+        """ Root Servers """
+        rootservers = []
+
+        print "We have found the following root servers:"
+
+        """ Join The Finds """
+        for server, operator, ip in zip(soup.findAll('td', attrs ={'id':'server'}), soup.findAll('td', attrs = {'id':'operator'}), soup.findAll('td', attrs = {'id':'ips'})):
+            rootserver = (server.find('a') or server).contents[0]
+            operator = operator.contents[0]
+            ip = ip.contents[0].split(":")[1]
+
+            rootservers.append({rootserver:{"operator":operator,"ip":ip}})
+            """ Print Out What We Found """
+            if(verbose):
+                    print '{0}.root-servers.org is operated by {1} and exists at {2}'.format(rootserver,operator,ip)
+
+        return rootservers
+
+print(getrootservers(url))
